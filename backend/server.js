c55a116840ace6823a66e5bc3bf7480a35c351d0
dada-mhmd <moudi.ld@hotmail.com>
2022-10-29 17:04:09 +0300

    added jwt and fixed some issues

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const notes = require('./data/notes')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 
 const app = express()
+app.use(cors())
 dotenv.config()
+connectDB()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running..')
@@ -13,11 +22,10 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const { id } = req.params
-  const note = notes.find(n => n._id === id)
-  res.json(note)
-})
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Port started on port ${PORT}`))
